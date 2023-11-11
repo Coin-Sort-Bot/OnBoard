6d3b8263f2b2cc69fc981cc752f8e98c13158ae4
Wolf Mermelstein <wolf.mermelstein@cwru.edu>
2023-11-11 16:25:23 -0500

    Add LED script

--- src/scripts/lights.py
+++ src/scripts/lights.py
@@ -7,7 +7,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 hat = adafruit_pca9685.PCA9685(i2c, address=0x41)
 
 hat.frequency = 60
-led_channel = hat.channels[0]
+led_channel = hat.channels[16]
 
 # Brightness ranges from 0 to 65535
 for i in range(65535):
