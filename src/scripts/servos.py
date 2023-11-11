320f589e365a26ace87f45a4687ab4a7bacb7f92
Wolf Mermelstein <wolf.mermelstein@cwru.edu>
2023-11-11 16:45:41 -0500

    Change servo address

--- src/scripts/servos.py
+++ src/scripts/servos.py
@@ -1,7 +1,7 @@
 from adafruit_servokit import ServoKit
 from time import sleep
 
-kit = ServoKit(channels=16, address=1)
+kit = ServoKit(channels=16, address=0x41)
 
 for i in range(16):
     kit.servo[i].angle = 0
