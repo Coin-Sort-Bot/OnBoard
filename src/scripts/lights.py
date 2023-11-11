a4697fe1907e661fabb31781275df809607b00cd
Wolf Mermelstein <wolf.mermelstein@cwru.edu>
2023-11-11 16:19:17 -0500

    Add LED script

---
+++ src/scripts/lights.py
@@ -0,0 +1,15 @@
+import board
+import busio
+import adafruit_pca9685
+from time import sleep
+
+i2c = busio.I2C(board.SCL, board.SDA)
+hat = adafruit_pca9685.PCA9685(i2c, address=0x41)
+
+hat.frequency = 60
+led_channel = hat.channels[0]
+
+# Brightness ranges from 0 to 65535
+for i in range(65535):
+    led_channel.duty_cycle = i
+    sleep(.001)
--- src/scripts/servos.py
+++ src/scripts/servos.py
@@ -1,10 +1,13 @@
 from adafruit_servokit import ServoKit
 from time import sleep
 
-kit = ServoKit(channels=16)
+kit = ServoKit(channels=16, address=1)
 
-kit.servo[0].angle = 0
-sleep(1)
-kit.servo[0].angle = 40
-sleep(1)
-kit.servo[0].angle = 0
+for i in range(16):
+    kit.servo[i].angle = 0
+    for ang in range(180):
+        kit.servo[i].angle = ang
+        sleep(0.05)
+    kit.servo[i].angle = 0
+    sleep(.1)
+
